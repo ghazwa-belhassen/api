62abcade574361893d15558158b689e90705dc75
ghazwa-belhassen <ghazouabelhassen02@gmail.com>
2023-07-12 12:19:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserList from './UserList';
+import './styles.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>My App</h1>
+      <UserList />
+    </div>
+  );
+};
+
+export default App;
